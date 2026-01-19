bf71ac2e5f0bd619a8fdcfe435974fe17381a5d7
JosephAnyen <156127114+JosephAnyen@users.noreply.github.com>
2026-01-19 14:42:50 +0200

    starting operations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import "./styles.css";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+}; //Action is adding the digits
+
+function reducer(state: any, { type, payload }: any) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+    default:
+      return state;
+  }
+} //types because we are going to different types of actions and payload is the data we are going to send along with the action
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: "1" } });
+
   return (
     <div className="calculator-Grid">
       <div className="output">
-        <div className="previous-Operand">1*454545</div>
-        <div className="current-Operand">1345455 </div>
+        <div className="previous-Operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-Operand">{currentOperand}</div>
       </div>
 
       <button>√</button>
